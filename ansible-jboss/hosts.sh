300faf2a18f9f9416197976e928f2b8fd9bec22d
frédéric ROSSI <frederic.rossi@numergy.com>
2014-10-08 17:20:22 +0200

    tested on prod openstack

--- ansible-jboss/hosts.sh
+++ ansible-jboss/hosts.sh
@@ -2,3 +2,4 @@
 
 
 echo "127.0.0.1 test-jboss" >> /etc/hosts
+echo "185.39.216.52 test-jboss" >> /etc/hosts
--- installAnsibleOnUbuntu.sh
+++ installAnsibleOnUbuntu.sh
@@ -7,11 +7,11 @@
 logfile=/tmp/ansible.log
 
 echo "Update repositories"
-sudo apt-get install -y software-properties-common
-sudo apt-add-repository ppa:ansible/ansible
+#sudo apt-get install -y software-properties-common
+#sudo apt-add-repository ppa:ansible/ansible
 apt-get update 2>$logfile 1>$logfile
 
-echo "Install puppet"
+echo "Install ansible"
 apt-get install -y ansible
 echo "Ansible version $(ansible --version) installed"
 
